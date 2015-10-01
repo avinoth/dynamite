95a4ec17433a033d8548b56b7432f71f42693641
Vinoth <a.vinothk@outlook.com>
2015-10-01 23:05:55 +0530

    Add relation

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,2 +1,4 @@
 class Debt < ActiveRecord::Base
+  has_many :transactions
+  has_may :parties
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ActiveRecord::Base
+  belongs_to :debt
+  belongs_to :party
 end
